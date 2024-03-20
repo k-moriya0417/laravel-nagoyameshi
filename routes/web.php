17bb0d9116023ced113898d3b67fce8f8b758626
mac <mac@macnoMacBook-Air.local>
2024-03-20 11:31:03 +0900

    Laravel Breezeパッケージを再インストール

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RestaurantController;
-use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\FavoriteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,11 +10,10 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -34,9 +32,3 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::resource('restaurants',RestaurantController::class);
-
-Route::post('reviews',[ReviewController::class,'store'])->name('reviews.store');
-
-Route::post('favorites/{restaurant_id}',[FavoriteController::class,'store'])->name('favorites.store');
-Route::delete('favorites/{restaurant_id}',[FavoriteController::class,'destroy'])->name('favorites.destroy');
-
